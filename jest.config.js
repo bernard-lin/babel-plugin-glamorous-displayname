ee8050a5d9a52c95b16a0c3a0b5e55a6a8514e45
Kent C. Dodds <kent@doddsfamily.us>
2017-06-12 16:53:13 -0600

    tests: add tests
    Closes #9

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+}
